53ac42bc3275ccf64e7e39b8f3182dcbd53bd8fc
Heyang Liu <lhypds@gmail.com>
2026-08-01 18:50:32 +0900

    Fix icon

--- web/src/components/InfoButton/InfoButton.jsx
+++ web/src/components/InfoButton/InfoButton.jsx
@@ -34,9 +34,10 @@ export default function InfoButton() {
     <>
       <ActionButton tooltip="hooks.json" ariaLabel="Show hooks.json" onClick={() => setOpen(true)}>
         <svg viewBox="0 0 24 24">
-          <circle cx="12" cy="12" r="9" />
-          <path d="M12 11v6" />
-          <path d="M12 7.5v.5" />
+          <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z" />
+          <path d="M14 2v6h6" />
+          <path d="M8 13h8" />
+          <path d="M8 17h8" />
         </svg>
       </ActionButton>
       <Modal isOpen={open} onClose={() => setOpen(false)} title="hooks.json" closeOnOverlay>
